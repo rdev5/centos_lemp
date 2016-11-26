74112f53d592dc18c07c935ccac4e4984d910f16
Matt Borja <matt.borja@gmail.com>
2016-11-25 20:34:25 -0700

    Create vhost.pp
    Define vhost manifest as resource to invoke multiple times

---
+++ manifests/resource/vhost.pp
@@ -0,0 +1,41 @@
+# Examples
+# --------
+#
+# @example
+#    centos_lemp::resource::vhost {
+#        site_conf => 'example.com.conf',
+#        site_template => 'centos_lemp/nginx.conf.default.erb',
+#        server_names => ['example.com', 'www.example.com'],
+#        root => '/var/www/example.com',
+#        index => ['index.html', 'index.htm'],
+#    }
+define centos_lemp::vhost (
+  $site_conf = undef,
+  $site_template = undef,
+  $server_names = [],
+  $www_root = undef,
+  $index = [],
+  
+) {
+  file { "${www_root}":
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
+  }
+
+  file { "/etc/nginx/sites-available/${site_conf}":
+    ensure => present,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    content => template("${site_template}"),
+    notify => Service['nginx'],
+  }
+  
+  file { "/etc/nginx/sites-enabled/${site_conf}":
+    ensure => 'link',
+    target => "/etc/nginx/sites-available/${site_conf}",
+    notify => Service['nginx'],
+  }
+}
